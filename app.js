b8d7de82d5daf7391c56ebcffdb4d4218a4cc753
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-19 23:44:40 +0200

    update

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-console.log('ciao');
+console.log('console della canvas');
 
-const myCanvas = document.getElementById('my-canvas');
+const myCanvas = document.getElementById('my-canvas'); 
 
-const ctx = myCanvas.getContext('2d');
+const ctx = myCanvas.getContext('2d'); //definiamo come vogliamo la tela 2D o 3D
 
 
-// ctx.strokeRect(20, 20, 50, 60); // RETTANGOLO --> //(x, y, with, height) //(cordinataX, cordinataY, Lunghezza, Altezza)
+// ctx.strokeRect(20, 20, 50, 60); // RETTANGOLO --> //(x, y, with, height)
 
 for (let i = 0; i < 100; i++) {
 
-    const originX = Math.random() * 600;
-    const originY = Math.random() * 600;
+    const originX = Math.random() * 500;    //create in this x area
+    const originY = Math.random() * 500;    //create in this y area
 
     const width = Math.random() * 100;
     const height = Math.random() * 100;
@@ -26,3 +26,8 @@ function getRandomColor() {
     }
     return color; 
   }
+
+
+  console.log((Math.random()));
+
+
