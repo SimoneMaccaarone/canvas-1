ece54fa2cf73a33e9c005209d5245be16ed7a6d3
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-20 17:33:56 +0200

    update

--- app.js
+++ app.js
@@ -3,34 +3,42 @@ const ctx = myCanvas.getContext('2d');
 
 
 //--- Creation Point/pixel
-const point ={
+const point = {
     x: 300,
-    y:300
+    y: 300
 }
 
+
 setInterval(() => {
-    
-    const randomX = (Math.random()*4)-2;
-    const randomY = (Math.random()*4)-2;
+
+    const randomX = (Math.random() * 4) - 2;
+    const randomY = (Math.random() * 4) - 2;
 
 
-    ctx.fillStyle='red' // pennarello rosso
-    ctx.fillRect(point.x,point.y, 2,2);
+    ctx.fillStyle = getRandomColorRGB();
+    ctx.fillRect(point.x, point.y, 2, 2);
 
     point.x += randomX;
     point.y += randomY;
-    
-}, 100);
- 
-for (let i = 0; i < 100; i++) {
 
-    
+}, 1);
+
+
+
+function getRandomColorRGB() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
+
+    return ctx.fillStyle = `rgba(${red},${green},${blue}`
 }
 
-// function getRandomColorRGB(){
-//     const red = Math.floor(Math.random()* 256);
-//     const green = Math.floor(Math.random()* 256);
-//     const blue = Math.floor(Math.random()* 256);
-//     const alpha = Math.random();
-//     ctx.strokeStyle = 
-//   }
+function getRandomColorRGBA() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
+
+    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
+}
--- old/app_old.js
+++ old/app_old.js
@@ -1,49 +1,31 @@
-console.log('console della canvas');
+console.log('la mia canvas');
 
-const myCanvas1 = document.getElementById('my-canvas1');
-const myCanvas2 = document.getElementById('my-canvas2');
+const myCanvas = document.getElementById('my-canvas');
 
+const ctx = myCanvas.getContext('2d');
 
-const ctx1 = myCanvas1.getContext('2d'); //definiamo come vogliamo la tela 2D o 3D
-const ctx2 = myCanvas2.getContext('2d');
-
-// ctx.strokeRect(20, 20, 50, 60); // RETTANGOLO --> //(x, y, with, height)
-
-ctx1.fillStyle = getRandomColorESA();
-const backGroundColor = ctx1.fillRect(0,0,500,500);  
 
 for (let i = 0; i < 100; i++) {
-
-    const originX = Math.random() * 500;    //create in this x area
-    const originY = Math.random() * 500;    //create in this y area
-
+    
+    const originX = Math.random() * 600;
+    //0.1 * 600 = 60
+    const originy = Math.random() * 600;
+    //0.3 * 600 = 180
     const width = Math.random() * 100;
+    //0.01 * 100 = 1
     const height = Math.random() * 100;
+    //0.9 * 100 = 90
+    
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
 
-    ctx1.fillStyle= getRandomColorESA();
-    ctx1.fillRect(originX, originY, width, height);
+    ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
 
+    ctx.fillRect(originX,originy,width,height);
 }
 //▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-// ctx2.strokeStyle= getRandomColorESA();
-createCircleInMIddleCross();
-creatCrossWithLine();
-
-function creatCrossWithLine(){
-ctx2.moveTo(0,0);
-ctx2.lineTo(500,500);
-ctx2.stroke();
-ctx2.moveTo(0, 500);
-ctx2.lineTo(500,0);
-ctx2.stroke();
-}
-
-function createCircleInMIddleCross(){
-  ctx2.arc(250, 250, 40, 0, 2 * Math.PI); // (x,y,raggio,)
-  ctx2.stroke();
-}
-
-
 
 //----- Random Color -----
 function getRandomColorESA() {
@@ -56,9 +38,10 @@ function getRandomColorESA() {
   }
 
   function getRandomColorRGB(){
-    const red = Math.floor(Math.random()* 256);
-    const green = Math.floor(Math.random()* 256);
-    const blue = Math.floor(Math.random()* 256);
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
     const alpha = Math.random();
-    ctx.strokeStyle = 
+
+    return ctx.fillStyle = `rgba(${red},${green},${blue},${alpha})`
   }
