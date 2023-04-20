b8194a8b08e2a87a1cd817543057044386e414c2
SunshineMocha <maccaronesimone03@gmail.com>
2023-04-20 12:31:11 +0200

    cross cirngici+ù

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 console.log('console della canvas');
 
-const myCanvas = document.getElementById('my-canvas'); 
+const myCanvas1 = document.getElementById('my-canvas1');
+const myCanvas2 = document.getElementById('my-canvas2');
 
-const ctx = myCanvas.getContext('2d'); //definiamo come vogliamo la tela 2D o 3D
 
+const ctx1 = myCanvas1.getContext('2d'); //definiamo come vogliamo la tela 2D o 3D
+const ctx2 = myCanvas2.getContext('2d');
 
 // ctx.strokeRect(20, 20, 50, 60); // RETTANGOLO --> //(x, y, with, height)
+ctx1.fillStyle = getRandomColor();
+const backGroundColor = ctx1.fillRect(0,0,500,500);  
 
 for (let i = 0; i < 100; i++) {
 
@@ -15,9 +19,32 @@ for (let i = 0; i < 100; i++) {
     const width = Math.random() * 100;
     const height = Math.random() * 100;
 
-    ctx.fillStyle= getRandomColor();
-    ctx.fillRect(originX, originY, width, height);
+    ctx1.fillStyle= getRandomColor();
+    ctx1.fillRect(originX, originY, width, height);
+
 }
+
+ctx2.strokeStyle= getRandomColor();
+creatCrossWithLine();
+
+function creatCrossWithLine(){
+ctx2.moveTo(0,0);
+ctx2.lineTo(500,500);
+ctx2.stroke();
+ctx2.moveTo(0, 500);
+ctx2.lineTo(500,0);
+ctx2.stroke();
+}
+
+function createCircleInMIddleCross(){
+  ctx2.beginPath();
+  ctx2.arc(95, 50, 40, 0, 2 * Math.PI)
+  ctx2.strokeStyle()
+}
+
+
+
+
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
